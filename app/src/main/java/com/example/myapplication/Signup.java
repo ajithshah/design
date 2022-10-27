16152ec1a4efb6bdea5362f946f64dc6f034b825
ajithshah <ajithshah37@gmail.com>
2022-10-27 10:22:11 +0530

    [Ajith] update

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        b1 = findViewById(R.id.signup);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(Signup.this,MainActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 }
